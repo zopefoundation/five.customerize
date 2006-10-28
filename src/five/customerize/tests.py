a544e353bfc69f8571bfa32aa8b4a28ac355531f
Alec Mitchell <alecpm@gmail.com>
2006-10-28 23:13:50 +0000

    Add initial classes for TTWTemplate and renderer

---
+++ src/five/customerize/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from Testing.ZopeTestCase import ZopeDocFileSuite
+from zope.traversing.adapters import DefaultTraversable
+import zope.component.testing
+
+__docformat__ = "reStructuredText"
+
+def setUp(test):
+    zope.component.testing.setUp(test)
+    zope.component.provideAdapter(DefaultTraversable, (None,))
+
+def test_suite():
+    return unittest.TestSuite([
+        ZopeDocFileSuite('zpt.txt', package="five.customerize",
+                         setUp=setUp, tearDown=zope.component.testing.tearDown),
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
---
+++ src/five/customerize/zpt.py
@@ -0,0 +1,37 @@
+from Products.PageTemplates.ZopePageTemplate import ZopePageTemplate
+
+class TTWTemplate(ZopePageTemplate):
+    """A template class used to generate Zope 3 views TTW"""
+
+    def __init__(self, id, text=None, content_type=None, encoding='utf-8',
+                 strict=False, view=None):
+        self.view = view
+        super(TTWTemplate, self).__init__(id, text, content_type, encoding,
+                                          strict)
+
+    def __call__(self, context, request):
+        return TTWTemplateRenderer(context, request, self, self.view)
+
+
+class TTWTemplateRenderer(object):
+    def __init__(self, context, request, template, view):
+        self.context = context
+        self.request = request
+        self.template = template
+        self.view = view
+
+    def __call__(self, *args, **kwargs):
+        """Add the zope user to the security context, as done in
+        PageTemplateFile"""
+        view = self.view
+        if view is not None:
+            class TTWView(view):
+                __allow_access_to_unprotected_subobjects__ = 1
+            view = TTWView(self.context, self.request)
+        bound_names = {'view': view,
+                       'request': self.request,
+                       'context': self.context}
+        return self.template._exec(bound_names, args, kwargs)
+
+    def __of__(self, obj):
+        return self
