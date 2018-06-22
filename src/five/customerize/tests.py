9ce47eb9ec3656cc140329455832af11bfddcd13
Philip Bauer <bauer@starzel.de>
2018-06-22 17:24:14 +0200

    fix tests for py3

--- src/five/customerize/tests.py
+++ src/five/customerize/tests.py
@@ -9,6 +9,18 @@ from zope.publisher.http import HTTPCharsets
 from zope.component.hooks import setHooks
 from Zope2.App.zcml import load_config
 
+import doctest
+import re
+import six
+
+
+class Py23DocChecker(doctest.OutputChecker):
+    def check_output(self, want, got, optionflags):
+        if six.PY2:
+            got = re.sub('Unauthorized', 'AccessControl.unauthorized.Unauthorized', got)
+            got = re.sub("u'(.*?)'", "'\\1'", got)
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
 
 def setUp(test):
     testing.setUp(test)
@@ -27,8 +39,12 @@ def test_suite():
     return TestSuite([
         ZopeDocFileSuite('zpt.txt', package="five.customerize",
                          setUp=setUp, tearDown=testing.tearDown),
-        ZopeDocFileSuite('customerize.txt', package="five.customerize",
-                         setUp=setUp),
+        ZopeDocFileSuite(
+            'customerize.txt',
+            package="five.customerize",
+            setUp=setUp,
+            checker=Py23DocChecker(),
+            ),
         FunctionalDocFileSuite('browser.txt', package="five.customerize",
                                setUp=setUp)
         ])
